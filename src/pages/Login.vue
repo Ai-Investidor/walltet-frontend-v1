ef56bc2fa4ec32f4bdc05bcb7ea5b276aaa9d8ce
jeffersonkl12 <jeffersonkl99@gmail.com>
2026-08-22 16:09:06 -0300

    fix: Ajuste tela login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -8,6 +8,6 @@ import PainelProva from '@views/login/PainelProva.vue'
     <main class="flex flex-1 flex-col justify-center">
       <Formulario />
     </main>
-    <PainelProva class="w-115 shrink-0 max-md:w-full" />
+    <PainelProva class="w-2/5 max-w-180 shrink-0 max-md:w-full max-md:max-w-none" />
   </div>
 </template>
